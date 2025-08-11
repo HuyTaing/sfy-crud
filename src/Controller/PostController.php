eeebdc2a9372ed4434cb04183a812059f606e09a
Huy TAING <taing.huy@outlook.fr>
2025-08-11 15:29:16 +0200

    Create Post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -17,6 +21,27 @@ final class PostController extends AbstractController
         // dd($posts);
         return $this->render('pages/post/index.html.twig', [
             'posts' => $posts
+        ]);
+    }
+
+    #[Route('/posts/create', name: 'posts_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('posts_index');
+        }
+
+        return $this->render('pages/post/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
